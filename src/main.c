0e180432608620e92ff31a9ab5e0da92a1a390eb
Yuri Cruz <yuriaicruz@gmail.com>
2023-05-28 17:22:51 -0300

    project config

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <genesis.h>
+
+int main()
+{
+    VDP_drawText("Hell-o World!", 2,13);
+
+    while(1)
+    {
+        //For versions prior to SGDK 1.60 use VDP_waitVSync instead.
+        SYS_doVBlankProcess();
+    }
+    return (0);
+}
